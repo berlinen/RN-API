7842b5d930f87266c080035adb34e1a79507a716
berlinen <1292393108@qq.com>
2019-10-10 17:36:45 +0800

    update

--- src/views/base/buttonBasics.js
+++ src/views/base/buttonBasics.js
@@ -57,6 +57,7 @@ export default function App() {
         <View style={styles.fixToText}>
           <Button
             title="Left button"
+            color="red"
             onPress={() => Alert.alert('Left button pressed')}
           />
           <Button
