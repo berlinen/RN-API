e4232e05e6bd7a59ab35c64c39b9338ee4f8a34a
berlinen <1292393108@qq.com>
2019-10-25 11:15:32 +0800

    移除tabbar

--- src/views/api/tabbar.tsx
+++ src/views/api/tabbar.tsx
@@ -1,3 +1,5 @@
+// 移除
+
 import React from 'react';
 import {
   StyleSheet,
@@ -16,26 +18,36 @@ const Tabbar: React.SFC = () => {
              barTintColor="black"    // TabBarIOS背景色}
              translucent={false}     // TabBarIOS不需要半透明效果
         >
+
             <TabBarIOS.Item
                 systemIcon="bookmarks"  // 系统图标(bookmarks)
             >
+              <Text>1</Text>
             </TabBarIOS.Item>
+
             <TabBarIOS.Item
                 systemIcon="contacts"  // 系统图标(contacts)
             >
+               <Text>1</Text>
             </TabBarIOS.Item>
+
             <TabBarIOS.Item
                 systemIcon="downloads"  // 系统图标(downloads)
             >
+               <Text>1</Text>
             </TabBarIOS.Item>
+
             <TabBarIOS.Item
                 systemIcon="favorites"  // 系统图标(favorites)
             >
+               <Text>1</Text>
             </TabBarIOS.Item>
+
             <TabBarIOS.Item
                 systemIcon="history"  // 系统图标(history)
                 badge="99+"
             >
+               <Text>1</Text>
             </TabBarIOS.Item>
         </TabBarIOS>
     </View>
